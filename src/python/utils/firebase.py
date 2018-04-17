e82b87bf4872c004caacb08069cae86ad637d7b0
Omar Abdelkader <omikader@gmail.com>
2018-04-17 16:23:23 -0400

    fix errors

--- src/python/utils/firebase.py
+++ src/python/utils/firebase.py
@@ -2,6 +2,6 @@ import json
 from firebase import firebase
 
 app = firebase.FirebaseApplication('https://i-robot-f4a0c.firebaseio.com/', None)
-result = firebase.get('/', None)
+result = app.get('/', None)
 
-print result.values[0].values[0]
+print result.values()[0].values()[0]

aaf43a78f59bfb6b54af8edb211bb35f060ffb60
Omar Abdelkader <omikader@gmail.com>
2018-04-17 16:14:52 -0400

    add firebase code

---
+++ src/python/utils/firebase.py
@@ -0,0 +1,7 @@
+import json
+from firebase import firebase
+
+app = firebase.FirebaseApplication('https://i-robot-f4a0c.firebaseio.com/', None)
+result = firebase.get('/', None)
+
+print result.values[0].values[0]
